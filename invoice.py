8540b9196296f4e4e727e835c67d6704508d41c2
Albert Cervera i Areny <albert@nan-tic.com>
2013-04-16 23:07:22 +0200

    pyflakes fixes.

--- invoice.py
+++ invoice.py
@@ -1,12 +1,12 @@
 #This file is part account_invoice_consecutive module for Tryton.
 #The COPYRIGHT file at the top level of this repository contains 
 #the full copyright notices and license terms.
-from trytond.model import Workflow, ModelView, ModelSQL
 from trytond.transaction import Transaction
-from trytond.pool import Pool, PoolMeta
+from trytond.pool import PoolMeta
 
 __all__ = ['Invoice']
 __metaclass__ = PoolMeta
+
 
 class Invoice:
     'Invoice'
@@ -26,7 +26,7 @@ class Invoice:
         # TODO: When do we check this?
         #if not invoice.journal_id.check_invoice_lines_tax:
             #continue
-        res = super(Invoice, self).set_number()
+        super(Invoice, self).set_number()
         if self.type in ('out_invoice', 'out_credit_note'):
             cursor = Transaction().cursor
             cursor.execute("""
