d9ec186853e505f1dae083d7d6972ccb8a82cb1a
Jordi Esteve <jesteve@zikzakmedia.com>
2014-08-31 02:14:37 +0200

    Improve error message with date format depending on transaction language

--- invoice.py
+++ invoice.py
@@ -2,7 +2,7 @@
 #The COPYRIGHT file at the top level of this repository contains
 #the full copyright notices and license terms.
 from trytond.transaction import Transaction
-from trytond.pool import PoolMeta
+from trytond.pool import Pool, PoolMeta
 
 __all__ = ['Invoice']
 __metaclass__ = PoolMeta
@@ -17,8 +17,8 @@ class Invoice:
         super(Invoice, cls).__setup__()
         cls._error_messages.update({
                 'invalid_number_date': 'You are trying to create '
-                    '%(invoice_number)s invoice, date %(invoice_date)s. '
-                    'There are %(invoice_count)d invoices before this date:'
+                    '%(invoice_number)s invoice on date %(invoice_date)s. '
+                    'There are %(invoice_count)d invoices after this date:'
                     '\n\n%(invoices)s',
                 })
 
@@ -26,6 +26,8 @@ class Invoice:
         # TODO: When do we check this?
         #if not invoice.journal_id.check_invoice_lines_tax:
             #continue
+        Lang = Pool().get('ir.lang')
+
         super(Invoice, self).set_number()
         if self.type in ('out_invoice', 'out_credit_note'):
             table = self.__table__()
@@ -41,15 +43,22 @@ class Invoice:
             cursor.execute(*query)
             records = cursor.fetchall()
             if records:
+                language = Transaction().language
+                languages = Lang.search([('code', '=', language)])
+                if not languages:
+                    languages = Lang.search([('code', '=', 'en_US')])
+                language = languages[0]
                 limit = 5
                 info = ['%(number)s - %(date)s' % {
                     'number': record[0],
-                    'date': unicode(record[1]),
+                    'date': Lang.strftime(record[1], language.code,
+                        language.date),
                     } for record in records]
                 info = '\n'.join(info[:limit])
                 self.raise_user_error('invalid_number_date', {
                     'invoice_number': self.number,
-                    'invoice_date': self.invoice_date,
+                    'invoice_date': Lang.strftime(self.invoice_date,
+                        language.code, language.date),
                     'invoice_count': len(records),
                     'invoices': info,
                     })
