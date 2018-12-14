4853b24fc1d493ba0be6e233d4a61d69c432c3c6
Raimon Esteve <raimon@nan-tic.com>
2018-12-14 10:06:32 +0100

    Replace self to invoice from >4.8 in date control when amount is round 0 From changeset-a67397bf91c1

--- invoice.py
+++ invoice.py
@@ -77,19 +77,19 @@ class Invoice:
 
             domain = [
                 ('number', '!=', None),
-                ('type', '=', self.type),
-                ('company', '=', self.company.id),
+                ('type', '=', invoice.type),
+                ('company', '=', invoice.company.id),
                 ('move.period.fiscalyear', '=', fiscalyear.id),
                 ['OR', [
-                        ('number', '<', self.number),
-                        ('invoice_date', '>', self.invoice_date),
+                        ('number', '<', invoice.number),
+                        ('invoice_date', '>', invoice.invoice_date),
                         ], [
-                        ('number', '>', self.number),
-                        ('invoice_date', '<', self.invoice_date),
+                        ('number', '>', invoice.number),
+                        ('invoice_date', '<', invoice.invoice_date),
                         ],],
                 ]
 
-            if self.untaxed_amount >= 0:
+            if invoice.untaxed_amount >= 0:
                 domain.append(('untaxed_amount', '>=', 0))
             else:
                 domain.append(('untaxed_amount', '<', 0))
@@ -100,7 +100,7 @@ class Invoice:
             ])
 
             if account_invoice_sequence_module_installed:
-                domain.append(('journal', '=', self.journal.id))
+                domain.append(('journal', '=', invoice.journal.id))
 
             invs = Inv.search(domain, limit=5)
 
