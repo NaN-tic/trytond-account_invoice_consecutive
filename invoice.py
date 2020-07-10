dcbd4370984069c0cd054d95046c87fa17e47fb8
Bernat Brunet <bernat@nan-tic.com>
2020-07-10 16:53:33 +0200

    Backport the sequence control used in v5.4 for the consecutive control check on same sequeunce

--- invoice.py
+++ invoice.py
@@ -2,6 +2,7 @@
 # the full copyright notices and license terms.
 from trytond.transaction import Transaction
 from trytond.pool import Pool, PoolMeta
+from trytond.model import fields
 from sql.aggregate import Sum
 from sql.functions import Round
 from trytond.i18n import gettext
@@ -11,6 +12,8 @@ __all__ = ['Invoice']
 
 class Invoice(metaclass=PoolMeta):
     __name__ = 'account.invoice'
+
+    sequence = fields.Many2One('ir.sequence.strict', "Sequence", readonly=True)
 
     @classmethod
     def validate(cls, invoices):
@@ -38,67 +41,118 @@ class Invoice(metaclass=PoolMeta):
                     accounting_date= Lang.strftime(self.accounting_date,
                         language.code, language.date)))
 
+    def get_next_number(self, pattern=None):
+        pool = Pool()
+        Sequence = pool.get('ir.sequence.strict')
+        Period = pool.get('account.period')
+
+        if pattern is None:
+            pattern = {}
+        else:
+            pattern = pattern.copy()
+
+        accounting_date = self.accounting_date or self.invoice_date
+        period_id = Period.find(
+            self.company.id, date=accounting_date,
+            test_state=self.type != 'in')
+
+        period = Period(period_id)
+        fiscalyear = period.fiscalyear
+        pattern.setdefault('company', self.company.id)
+        pattern.setdefault('fiscalyear', fiscalyear.id)
+        pattern.setdefault('period', period.id)
+        invoice_type = self.type
+        invoice_type += self.invoice_type_criteria()
+
+        for invoice_sequence in fiscalyear.invoice_sequences:
+            if invoice_sequence.match(pattern):
+                sequence = getattr(
+                    invoice_sequence, '%s_sequence' % invoice_type)
+                break
+        else:
+            raise InvoiceNumberError(
+                gettext('account_invoice.msg_invoice_no_sequence',
+                    invoice=self.rec_name,
+                    fiscalyear=fiscalyear.rec_name))
+        with Transaction().set_context(date=accounting_date):
+            return Sequence.get_id(sequence.id), sequence
+
     @classmethod
     def set_number(cls, invoices):
-        # TODO: When do we check this?
-        # if not invoice.journal_id.check_invoice_lines_tax:
-            # continue
+        '''
+        Set number to the invoice
+        '''
         pool = Pool()
-        Period = pool.get('account.period')
+        Date = pool.get('ir.date')
         Lang = pool.get('ir.lang')
+        Period = pool.get('account.period')
         Module = pool.get('ir.module')
-        Inv = pool.get('account.invoice')
+        today = Date.today()
 
-        to_check = [i for i in invoices if i.type == 'out' and not i.number]
+        def accounting_date(invoice):
+            return invoice.accounting_date or invoice.invoice_date or today
 
-        super(Invoice, cls).set_number(invoices)
-        for invoice in to_check:
-            # As we have a control in the validate that make the
-            # accounting_date have to be the same as invoice_date, in cas it
-            # exist, we can use invoice_date to calculate the period.
-            period_id = Period.find(
-                invoice.company.id, date=invoice.invoice_date)
-            fiscalyear = Period(period_id).fiscalyear
+        invoices = sorted(invoices, key=accounting_date)
+        sequences = set()
 
-            domain = [
-                ('number', '!=', None),
-                ('type', '=', invoice.type),
-                ('company', '=', invoice.company.id),
-                ('move.period.fiscalyear', '=', fiscalyear.id),
-                ['OR', [
-                        ('number', '<', invoice.number),
-                        ('invoice_date', '>', invoice.invoice_date),
-                        ], [
-                        ('number', '>', invoice.number),
-                        ('invoice_date', '<', invoice.invoice_date),
-                        ],],
-                ]
+        for invoice in invoices:
+            # Posted and paid invoices are tested by check_modify so we can
+            # not modify tax_identifier nor number
+            if invoice.state in {'posted', 'paid'}:
+                continue
+            if not invoice.tax_identifier:
+                invoice.tax_identifier = invoice.get_tax_identifier()
+            # Generated invoice may not fill the party tax identifier
+            if not invoice.party_tax_identifier:
+                invoice.party_tax_identifier = invoice.party.tax_identifier
 
-            if invoice.untaxed_amount >= 0:
-                domain.append(('untaxed_amount', '>=', 0))
-            else:
-                domain.append(('untaxed_amount', '<', 0))
+            if invoice.number:
+                continue
 
-            account_invoice_sequence_module_installed = Module.search([
-                    ('name', '=', 'account_invoice_multisequence'),
-                    ('state', '=', 'activated'),
-            ])
+            if not invoice.invoice_date and invoice.type == 'out':
+                invoice.invoice_date = today
+            invoice.number, invoice.sequence = invoice.get_next_number()
+            if invoice.type == 'out' and invoice.sequence not in sequences:
+                date = accounting_date(invoice)
+                period_id = Period.find(
+                    invoice.company.id, date=invoice.invoice_date)
+                fiscalyear = Period(period_id).fiscalyear
+                # Do not need to lock the table
+                # because sequence.get_id is sequential
+                domain = [
+                    ('sequence', '=', invoice.sequence.id),
+                    ('move.period.fiscalyear', '=', fiscalyear.id),
+                    ['OR', [
+                            ('number', '<', invoice.number),
+                            ('invoice_date', '>', invoice.invoice_date),
+                            ], [
+                            ('number', '>', invoice.number),
+                            ('invoice_date', '<', invoice.invoice_date),
+                            ],],
+                    ]
 
-            if account_invoice_sequence_module_installed:
-                domain.append(('journal', '=', invoice.journal.id))
+                account_invoice_sequence_module_installed = Module.search([
+                        ('name', '=', 'account_invoice_multisequence'),
+                        ('state', '=', 'activated'),
+                ])
 
-            invs = Inv.search(domain, limit=5)
+                if account_invoice_sequence_module_installed:
+                    domain.append(('journal', '=', invoice.journal.id))
 
-            if invs:
-                language = Lang.get()
-                info = ['%(number)s - %(date)s' % {
-                    'number': inv.number,
-                    'date': language.strftime(inv.invoice_date),
-                    } for inv in invs]
-                info = '\n'.join(info)
-                raise UserError(gettext(
-                    'account_invoice_consecutive.invalid_number_date',
-                        invoice_number=invoice.number,
-                        invoice_date=language.strftime(invoice.invoice_date),
-                        invoice_count=len(invs),
-                        invoices=info))
+                after_invoices = cls.search(domain, limit=5)
+
+                if after_invoices:
+                    language = Lang.get()
+                    info = ['%(number)s - %(date)s' % {
+                        'number': after_invoice.number,
+                        'date': language.strftime(after_invoice.invoice_date),
+                        } for after_invoice in after_invoices]
+                    info = '\n'.join(info)
+                    raise UserError(gettext(
+                        'account_invoice_consecutive.invalid_number_date',
+                            invoice_number=invoice.number,
+                            invoice_date=language.strftime(invoice.invoice_date),
+                            invoice_count=len(after_invoices),
+                            invoices=info))
+                sequences.add(invoice.sequence)
+        cls.save(invoices)
